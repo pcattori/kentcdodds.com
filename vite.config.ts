4f6f3a4f44b17c8e2f3662a15bec1dd1dabac281
Pedro Cattori <pcattori@gmail.com>
2023-12-15 11:15:12 -0500

    ignore expected warnings about redundant dynamic imports for .server

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import {unstable_vitePlugin as remix} from '@remix-run/dev'
-import {defineConfig} from 'vite'
+import {createLogger, defineConfig} from 'vite'
 import tsconfigPaths from 'vite-tsconfig-paths'
 import {flatRoutes} from 'remix-flat-routes'
 
 export default defineConfig(() => {
   return {
+    customLogger: customLogger(),
     plugins: [
       remix({
         ignoredRouteFiles: ['**/*'],
@@ -29,3 +30,18 @@ export default defineConfig(() => {
     },
   }
 })
+
+function customLogger() {
+  const logger = createLogger()
+  // eslint-disable-next-line @typescript-eslint/unbound-method
+  const loggerWarn = logger.warn
+
+  const dotServerDynamicImportRE =
+    /\.server.*is dynamically imported by.*but also statically imported by/
+
+  logger.warn = (msg, options) => {
+    if (dotServerDynamicImportRE.test(msg)) return
+    loggerWarn(msg, options)
+  }
+  return logger
+}
